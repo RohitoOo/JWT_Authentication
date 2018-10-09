e6734d06ff5021d4454167894323e73d4c84bc71
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-10-09 17:08:35 +0530

    Passport & Google Strategy Setup

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,21 +1,25 @@
 const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/login' , (req,res) => {
     res.render('login')
 })
 
 router.get('/google' , (req,res) => {
-
     res.send("Loggin in with Google")
+})
 
+router.get('/facebook' , (req,res) => {
+    res.send("Loggin in with facebook")
+})
+
+router.get('/github' , (req,res) => {
+    res.send("Loggin in with github")
 })
 
 router.get('/logout' , (req,res) => {
-
     res.send("Logging Out")
-
 })
 
 module.exports = router
