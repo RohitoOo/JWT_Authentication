347795b0a9b3e89d719ec7bf4c2a66c1b620e788
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-10-10 18:14:20 +0530

    Heroku

---
+++ config/keys.js
@@ -0,0 +1,16 @@
+// DO NO UPLOAD TO GITHUB
+
+module.exports = {
+    google: {
+        clientID: '513930397858-b5fqk3jr0ki23u66uplbhdg8hum763lf.apps.googleusercontent.com',
+        clientSecret: 'gk0vyymgPygVznYw-IQr_F4k'
+    },
+    mongodb:  {
+        dbURI: 'mongodb://rohito:test12345@ds139960.mlab.com:39960/oauth'
+    },
+    //Cookie Session 
+    session:
+    {
+        cookieKey: "rohitobhambhani"
+    }
+}
